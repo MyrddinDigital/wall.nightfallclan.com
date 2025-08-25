c3e670806f9d53b103e74b3df7d0961c10971323
BluNomad <jltarheel47@gmail.com>
2025-08-24 22:55:57 -0400

    add debug logging

--- netlify/functions/roblox-proxy.js
+++ netlify/functions/roblox-proxy.js
@@ -1,5 +1,6 @@
 export default async (request, context) => {
   try {
+    console.log("got request:", request);
     const { pathname, search } = new URL(request.url);
     const proxiedUrl = new URL(`https://thumbnails.roblox.com/v1${pathname.replace('/.netlify/functions/roblox-proxy', '')}${search}`);
     
@@ -11,9 +12,10 @@ export default async (request, context) => {
       method: request.method,
     });
     
+    console.log("sending response:", response);
     return response;
   } catch (error) {
-    console.error(error);
+    console.error("got error:", error);
     return new Response(error.toString(), {
       status: 500,
     });
